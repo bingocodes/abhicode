5802e90507e06bf113c087f0027e298b4a103a0f
abhishekpandey2 <69098722+abhishekpandey2@users.noreply.github.com>
2020-08-02 09:26:12 +0530

    python codes
    all my python codes

---
+++ area.py
@@ -0,0 +1,16 @@
+print ('welcome')
+print('Friends this program is to help you finding area of rectangle figure')
+
+print('please follow the steps')
+
+print('enter lenth of the figure ')
+
+a1= input()
+
+print('enter the breadth of the figure')
+
+a2=input()
+
+print('the area is =')
+
+print(int(a1)*int(a2))
